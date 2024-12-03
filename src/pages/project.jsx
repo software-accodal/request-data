af35e2e14ea95d901b2490108f74310d8d286510
software-accodal <software@accodal.com>
2024-12-03 14:34:53 +0800

    Update project.jsx

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -333,7 +333,7 @@ function Projects({ email }) {
             title="Create Project Form"
             style={{
               width: "100%",
-              height: "100%", 
+              height: "90%", 
               border: "none",
             }}
           ></iframe>
