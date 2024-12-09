55ac233a8fc30fb05c3539d5c08cb79a3b0e9bbf
Mark Paulo Cruz <cruzmarkpaulo5@gmail.com>
2024-12-09 14:01:30 +0800

    fix module error
    fix error

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -34,8 +34,8 @@ export default function Docs() {
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-gray-50">
                             <tr>
-                                {docsCols.map((col) => (
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                {docsCols.map((col, index) => (
+                                    <th key={index} className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         {col}
                                     </th>
                                 ))}
