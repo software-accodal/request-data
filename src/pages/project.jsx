f48c8c58248c4584f3b2bec0ca13746f7a870578
software-accodal <software@accodal.com>
2024-12-11 23:12:31 +0800

    null

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -23,7 +23,7 @@ const getByFormula = async (formula) => {
   );
   const data = res.data;
   console.log(formula, data);
-  if (!data?.length) return null;
+  // if (!data) return null;
   const sortedData = data.sort((a, b) => {
     const dateA = new Date(a.fields["Created"]);
     const dateB = new Date(b.fields["Created"]);
